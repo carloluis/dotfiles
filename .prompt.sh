b3808af8ec008bb2f2d4d46393d0d36fa2d17a0b
carloluis <carloluisr@gmail.com>
2018-01-28 23:37:07 -0300

    update prompt git info..

--- .prompt.sh
+++ .prompt.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 function prompt_git() {
-	local branchName='';
-	local s='';
+	local branchname='';
+	local on=${1:-"${white} on ${violet}"}
+	local statuscolor=${2:-"${blue}"}
+	local status='';
 
 	# check if the current directory is in a git repository
 	if [ $(git rev-parse --is-inside-work-tree &>/dev/null; echo "${?}") == '0' ]; then
@@ -15,38 +17,38 @@ function prompt_git() {
 
 			# check for uncommitted changes in the index
 			if ! $(git diff --quiet --ignore-submodules --cached); then
-				s+='+';
+				status+='+';
 			fi;
 
 			# check for unstaged changes
 			if ! $(git diff-files --quiet --ignore-submodules --); then
-				s+='!';
+				status+='!';
 			fi
 
 			# check for untracked files
 			if [ -n "$(git ls-files --others --exclude-standard)" ]; then
-				s+='?';
+				status+='?';
 			fi;
 
 			# check for stashed files
 			if $(git rev-parse --verify refs/stash &>/dev/null); then
-				s+='$';
+				status+='$';
 			fi
 		fi
 
 		# get the short symbolic ref.
 		# if HEAD isn't a symbolic ref, get the short SHA for the latest commit
 		# otherwise, just give up
-		branchName="$(git symbolic-ref --quiet --short HEAD 2>/dev/null || \
+		branchname="$(git symbolic-ref --quiet --short HEAD 2>/dev/null || \
 			git rev-parse --short HEAD 2>/dev/null || \
 			echo '(unknown)')";
 
 		# ... or simply do
-		# branchName="$(git branch 2>/dev/null | grep '^*' | colrm 1 2)"
+		# branchname="$(git branch 2>/dev/null | grep '^*' | colrm 1 2)"
 
-		[ -n "${s}" ] && s=" [${s}]";
+		[ -n "${status}" ] && status=" [${status}]";
 
-		echo -e "${1}${branchName}${2}${s}";
+		echo -e "${on}${branchname}${statuscolor}${status}";
 	else
 		return;
 	fi
@@ -74,7 +76,7 @@ PS1+="\[${white}\] at ";
 PS1+="\[${yellow}\]\h";
 PS1+="\[${white}\] in ";
 PS1+="\[${green}\]\W";
-PS1+="\$(prompt_git \"\[${white}\] on \[${violet}\]\" \"\[${blue}\]\")"
+PS1+="\$(prompt_git)"
 PS1+="\n";
 PS1+="\[${gray}\]\[\e(0\]mq\[\e(B\] ";
 PS1+="\[${white}\]\$ ";
