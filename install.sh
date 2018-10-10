551d75f8deeff3765426e352c356e8938c7d925f
Carloluis Rodriguez <carloluisr@gmail.com>
2018-10-11 00:46:52 +0200

    using absolute paths on install script (#8)
    - update log messages...

--- install.sh
+++ install.sh
@@ -4,16 +4,23 @@ DOTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 files=".bashrc .bash_profile .aliases.sh .functions.sh .prompt.sh"
 BACKUP_DIR="$DOTS_DIR/backup"
 
-echo "current dir: $DOTS_DIR"
+printf "\nCopying new dotfiles from $DOTS_DIR...\n"
 mkdir -p "$BACKUP_DIR"
+
+saved_files=0
 
 for file in $files; do
 	if [[ -f ~/"$file" ]]; then
-		echo "-- backup $file --"
 		mv -v ~/"$file" "$BACKUP_DIR"
+		((saved_files+=1))
 	fi
-	echo "--> ~/$file"
-	cp "$file" ~
+	cp "$DOTS_DIR/$file" ~
+	echo "  📝 --> ~/$file"
 done
+
+if [[ $saved_files -gt 0 ]]; then
+	echo -e "\nSaved $saved_files files on $BACKUP_DIR:"
+	ls -lAh $BACKUP_DIR
+fi
 
 source ~/.bashrc
